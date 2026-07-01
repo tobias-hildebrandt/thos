12252c5affd09deaac7ec5185d6f130b8a6d9a12
Tobias Hildebrandt <tobiash@tobiash.dev>
2026-07-01 13:22:12 +0200

    clarify REGISTER macro

--- src/kernel/asm.h
+++ src/kernel/asm.h
@@ -5,4 +5,6 @@
     #instr " " #reg ", 8 * (" #offset ")(" #base ")\n"
 #define REGISTER_MEM_PREFIX(instr, base, start_offset, prefix, x) \
     REGISTER_MEM(instr, base, prefix##x, start_offset + x)
+
+// declare a register variable
 #define REGISTER(REGISTER) register long REGISTER __asm__(#REGISTER)
